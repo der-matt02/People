43f1293e1559cbb50530b66e692681df5af9b1b2
der-matt02 <dermatt02@gmail.com>
2024-06-27 08:34:52 -0500

    Configuracion Inicial

---
+++ People/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using People.Repositories;
+namespace People;
+
+public partial class App : Application
+{
+    // TODO: Add a public static PersonRepository property
+    public static PersonRepository PersonRepo { get; private set; }
+
+    public App(PersonRepository repo)
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+
+        // TODO: Initialize the PersonRepository property with the PersonRespository singleton object
+        PersonRepo = repo;
+    }
+}
---
+++ People/Repositories/PersonRepository.cs
@@ -0,0 +1,69 @@
+﻿using SQLite;
+using People.Models;
+namespace People.Repositories;
+
+public class PersonRepository
+{
+    string _dbPath;
+
+    public string StatusMessage { get; set; }
+
+    // TODO: Add variable for the SQLite connection
+    private SQLiteConnection conn;
+
+    private void Init()
+    {
+        // TODO: Add code to initialize the repository
+        if (conn != null)
+            return;
+
+        conn = new SQLiteConnection(_dbPath);
+        conn.CreateTable<Person>();
+    }
+
+    public PersonRepository(string dbPath)
+    {
+        _dbPath = dbPath;
+    }
+
+    public void AddNewPerson(string name)
+    {
+        int result = 0;
+        try
+        {
+            // TODO: Call Init()
+            Init();
+
+            // basic validation to ensure a name was entered
+            if (string.IsNullOrEmpty(name))
+                throw new Exception("Valid name required");
+
+            // TODO: Insert the new person into the database
+            result = conn.Insert(new Person { Name = name });
+
+            StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+        }
+        catch (Exception ex)
+        {
+            StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
+        }
+
+    }
+
+    public List<Person> GetAllPeople()
+    {
+        // TODO: Init then retrieve a list of Person objects from the database into a list
+        try
+        {
+            Init();
+            return conn.Table<Person>().ToList();
+
+        }
+        catch (Exception ex)
+        {
+            StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+        }
+
+        return new List<Person>();
+    }
+}
